9634a7ce27d85377c94d149d3dfe363d23872456
Pradhuman19 <pradhumngupta14@gmail.com>
2025-08-06 18:23:41 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://pin-magic-backend.onrender.com/api",
+        target: "https://pin-magic-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
