03612c5a7eb0a8369834efba91b89a7499acebcb
Pradhuman19 <pradhumngupta14@gmail.com>
2025-08-16 20:10:17 +0530

    grid5

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       ) : (
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-2 m-4">
+            <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-5 gap-2 m-4">
               {pins && pins.length > 0 ? (
                 pins.map((e, i) => (
                   <div key={i} className="mb-2 break-inside-avoid">
